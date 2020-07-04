b4c46cb40f6be5b44a1bfdf4106c51a74f6d50f4
abdou155 <44580324+abdou155@users.noreply.github.com>
2020-07-04 14:20:53 +0100

    done

--- models/special.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const schema = mongoose.Schema ;
-
-const specialSchema = new schema({
-    speciality : String
-})
-
-module.exports =  mongoose.model('special', specialSchema);
